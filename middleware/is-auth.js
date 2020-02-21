f520bf98b843c394aca7ce4cf17f946eb8a22440
Damian Obajtek <damian.obajtek@gmail.com>
2020-02-21 21:40:01 +0100

    added auth and finish backend

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -18,4 +18,11 @@ module.exports = (req, res, next) => {
     req.isAuth = false;
     return next();
   }
+  if (!decodedToken) {
+    req.isAuth = false;
+    return next();
+  }
+  req.isAuth = true;
+  req.userId = decodedToken.userId;
+  next();
 };
