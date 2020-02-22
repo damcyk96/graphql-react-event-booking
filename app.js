bd6daf8f96e1769f4ec37cf81e54cc9234270741
Damian Obajtek <damian.obajtek@gmail.com>
2020-02-22 20:50:22 +0100

    fix warning node.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-nkgqa.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true }
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
     app.listen(8000);
