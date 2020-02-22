65db7054b88998a2a083f16e4d9fe27471b0df88
Damian Obajtek <damian.obajtek@gmail.com>
2020-02-22 18:48:52 +0100

    added event button with style

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 
+import "./Events.css";
+
 class EventsPage extends Component {
   render() {
-    return <h1>Events Page</h1>;
+    return (
+      <div className="events-control">
+        <p>Share your own Events!</p>
+        <button className="btn">Create Event</button>
+      </div>
+    );
   }
 }
 
