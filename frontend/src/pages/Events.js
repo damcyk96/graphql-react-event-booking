f7db762ea1504a6b836bee0733135219f616f7a6
Damian Obajtek <damian.obajtek@gmail.com>
2020-02-22 19:04:32 +0100

    add event with css

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 
+import Modal from "../components/Modal/Modal";
+
 import "./Events.css";
 
 class EventsPage extends Component {
   render() {
     return (
-      <div className="events-control">
-        <p>Share your own Events!</p>
-        <button className="btn">Create Event</button>
-      </div>
+      <React.Fragment>
+        <Modal title="Add Event" canCancel canConfirm>
+          <p>Modal content</p>
+        </Modal>
+        <div className="events-control">
+          <p>Share your own Events!</p>
+          <button className="btn">Create Event</button>
+        </div>
+      </React.Fragment>
     );
   }
 }
